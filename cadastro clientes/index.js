9b47e11369b7996e9ab9b94ced04e8b57e777a5f
dadriano09 <52056353+dadriano09@users.noreply.github.com>
2023-06-16 09:41:14 -0300

    Add files via upload

--- cadastro clientes/index.js
+++ cadastro clientes/index.js
@@ -1,52 +1,48 @@
 function setCookie(name, value, days) {
-    var expires = "";
-    if (days) {
-      var date = new Date();
-      date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-      expires = "; expires=" + date.toUTCString();
-    }
-    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+  var expires = "";
+  if (days) {
+    var date = new Date();
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    expires = "; expires=" + date.toUTCString();
   }
-  
-  function getCookie(name) {
-    var nameEQ = name + "=";
-    var ca = document.cookie.split(';');
-    for (var i = 0; i < ca.length; i++) {
-      var c = ca[i];
-      while (c.charAt(0) === ' ') {
-        c = c.substring(1, c.length);
-      }
-      if (c.indexOf(nameEQ) === 0) {
-        return c.substring(nameEQ.length, c.length);
-      }
+
+  document.cookie = name + "=" + value + ";" + expires + ";path=/";
+
+}
+
+function getCookie(name) {
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(';');
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1, c.length);
     }
-    return null;
-  }
-  
-  function handleFormSubmit(event) {
-    event.preventDefault();
-  
-    try {
-      var nome = document.getElementById("nome").value;
-      var id = document.getElementById("id").value;
-      var tipoCliente = document.getElementById("tipoCliente").value;
-      var endereco = document.getElementById("endereco").value;
-      var cep = document.getElementById("cep").value;
-      var dataNascimento = document.getElementById("dataNascimento").value;
-      var vendedor = document.getElementById("vendedor").value;
-      var limiteCredito = document.getElementById("limiteCredito").value;
-  
-
-      setCookie("nomeFormulario", nome, 30);
-      setCookie("idFormulario", id, 30);
-
-      alert("Formulário enviado com sucesso!");
-
-    } catch (error) {
-
-      alert("Ocorreu um erro ao enviar o formulário. Por favor, verifique os campos e tente novamente.");
-      console.error(error);
+    if (c.indexOf(nameEQ) == 0) {
+      return c.substring(nameEQ.length, c.length);
     }
   }
-  
-  document.getElementById("cadastroForm").addEventListener("submit", handleFormSubmit);
+  return "";
+}
+
+function handleFormSubmit(event) {
+  event.preventDefault();
+
+  try {
+    var nome = document.getElementById("nome").value;
+    var id = document.getElementById("id").value;
+
+
+    setCookie("nomeFormulario", nome, 30);
+    setCookie("idFormulario", id, 30);
+
+    alert("Formulário enviado com sucesso!");
+
+  } catch (error) {
+
+    alert("Ocorreu um erro ao enviar o formulário. Por favor, verifique os campos e tente novamente.");
+    console.error(error);
+  }
+}
+
+document.getElementById("cadastroForm").addEventListener("submit", handleFormSubmit);
